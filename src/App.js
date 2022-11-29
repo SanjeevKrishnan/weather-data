b1079fa81038bbf53416b7f3629a7df72509317d
Sanjeev Krishnan R <sanjeev.krishnan.r@gmail.com>
2022-11-29 19:15:29 +0530

    Update#2

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
 import axios from "axios";
 import React from "react";
 import GetWeather from "./getWeather";
+import "./App.css"
 
-const baseURL = "https://countriesnow.space/api/v0.1/countries";
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
+
 
 export default function App() {
   const [post, setPost] = React.useState(null);
   const [country, setCountry] = React.useState(null);
   const [openWeather, setOpenWeather] = React.useState(false);
+  const baseURL = "https://countriesnow.space/api/v0.1/countries";
+
+  const options = [
+    'one', 'two', 'three'
+  ];
+  const defaultOption = options[0];
+  
 
   React.useEffect(() => {
     axios.get(baseURL).then((response) => {
       setPost(response.data.data);
     });
   }, []);
+  const onClickofCountry = () => {
+    setOpenWeather(true);
+    setCountry(one_post.country);
+  } 
 
   if (!post) return null;
 
   return (
     <div>
-      <h1>Weather-APP</h1>
-      {post.map(one_post => {
-        return(
-          <div>
-            <div key={one_post.id} onClick={() => {
-                setOpenWeather(true)
-                setCountry(one_post.country);
-              }}>
-              {one_post.country}
-            </div>
-          </div>
-        );
-      })}
+      <h1 className="heading">Weather-APP</h1>
+      <Dropdown 
+              options={post} 
+              onChange={onClickofCountry}
+              value={post[0]} 
+              placeholder="Select an option" />
       {openWeather ? <GetWeather country = {country}/> : null}
     </div>
   );
--- src/getWeather.js
+++ src/getWeather.js
@@ -2,35 +2,41 @@ import axios from "axios";
 import React from "react";
 
 const GetWeather = (props) => {
-
+    console.log("inside", props.country)
     const [weather, setWeather] = React.useState(null);
     const weatherAPI = "https://api.openweathermap.org/data/2.5/weather?q="+props.country+"&APPID=794ee95e63c5a32aaf88cd813fa2e425";
 
     React.useEffect(() => {
-        axios.get(weatherAPI).then((response) => {
-            console.log("inside")
-            const data = response.data
+        console.log("useEffect called");
+        const getWeatherAPI = async () => {
+          try {
+            const res = await axios.get(weatherAPI);
+            const data = res.data
             setWeather({
-              "temp": {
-                "min": data.main.temp_min,
-                "max": data.main.temp_max,
-                "avg": data.main.temp
-              },
-              "humidity": data.main.humidity,
-              "wind": {
-                "deg": data.wind.deg,
-                "gust": data.wind.gust,
-                "speed": data.wind.speed
-              }
-            });
-          }).catch(
-          );
-      }, []);
-    
+                "temp": {
+                  "min": data.main.temp_min,
+                  "max": data.main.temp_max,
+                  "avg": data.main.temp
+                },
+                "humidity": data.main.humidity,
+                "wind": {
+                  "deg": data.wind.deg,
+                  "gust": data.wind.gust,
+                  "speed": data.wind.speed
+                }
+              });          
+            } catch (e) {
+            console.log(e);
+          }
+        };
+        getWeatherAPI();
+      }, [props.country]);
+      console.log(weather);
 
     return(
         <div>
-            <h1>{weather.temp.min}</h1>
+            <h2>Hello from {props.country}</h2>
+            <h1>{weather && weather.temp.min}</h1>
         </div>
     )
   }
